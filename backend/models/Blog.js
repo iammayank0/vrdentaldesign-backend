939331e242280a66925a09a94843c0473d2e3128
Mayank <2020kucp1006@iiitkota.ac.in>
2024-06-17 14:16:21 +0530

    ok

--- backend/models/Blog.js
+++ backend/models/Blog.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
-const BlogSchema = new mongoose.Schema({
+const BlogTextSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  heading: { type: String, required: true },
+  description: { type: String, required: true }
+});
+
+const BlogSchema = new mongoose.Schema({
+  BlogTitle: { type: String, required: true },
   date: { type: String, required: true }  ,
   image: { type: String, required: true },
   link: { type: String, required: true }
 });
 
+const BlogText = mongoose.model('BlogText', BlogTextSchema);
 const Blog = mongoose.model('Blog', BlogSchema);
 
-module.exports = { Blog };
+module.exports = { BlogText, Blog };
